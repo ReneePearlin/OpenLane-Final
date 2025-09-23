dde6fe1593a04ae27df9a27a31b8e53cf44e22eb
ReneePearlin <reneepearlin@gmail.com>
2025-09-23 23:11:36 +0530

    Waste'u

--- designs/my_project/runs/test13/config_in.tcl
+++
@@ -1,7 +0,0 @@
-set ::env(DESIGN_NAME) picorv32_wrapper
-
-# Point to both source files explicitly (not just glob, to avoid surprises)
-set ::env(VERILOG_FILES) "\
-    $::env(DESIGN_DIR)/src/picorv32.v \
-    $::env(DESIGN_DIR)/src/picorv32_wrapper.v"
-
